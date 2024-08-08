505ded7f6a923e1ef6b99a694578f40f42a55424
Fw3325 <146174404+Fw3325@users.noreply.github.com>
2024-08-08 14:54:58 -0400

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,7 @@
+class configure:
+    web_task_goal = "extract all the information about the job from the job posting in the original link"
+    web_agent_role = 'Data Scientist Job Researcher'
+    web_agent_goal = 'Do amazing analysis and extraction on job posting to help job applicants'
+    web_agent_bk = "As a Job Researcher, your prowess in navigating and extracting critical infomation from \
+            job postings is unmatched. Your skills help pinpoint the necessary qualifications and skills \
+            sought by employers, forming the foundation for effective application tailoring."
